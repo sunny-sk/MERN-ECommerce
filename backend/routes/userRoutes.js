0d3def34052696ab9fa7a7c1196724fdd84bb4fa
sunny-sk <sk956223@gmail.com>
2020-12-05 16:23:28 +0530

    user method and routes added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,14 @@ import {
   authUser,
   getUerProfile,
   registerUser,
+  updateUerProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/auth.js';
 
-router.route('/profile').get(protect, getUerProfile);
+router
+  .route('/profile')
+  .get(protect, getUerProfile)
+  .put(protect, updateUerProfile);
 router.post('/login', authUser);
 router.post('/register', registerUser);
 
