35ba711ad6fd4587c4b6daa8eb72efa4805ac31d
sunny-sk <sk956223@gmail.com>
2020-12-29 19:30:09 +0530

    image upload functionality added

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,23 @@ import express from 'express';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import cors from 'cors';
+import path from 'path';
 const app = express();
 dotenv.config();
 connectDB();
 app.use(cors());
 app.use(express.json());
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 //routes
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { errorHandler } from './middleware/error.js';
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/upload', uploadRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID),
